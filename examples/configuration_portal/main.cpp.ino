54ce4bc1342bfbbd5bdf34a1642b2490728be178
Marcelo Miranda <marceloc312@gmail.com>
2025-12-24 11:58:30 -0300

    feat: Implementa portal de configuração com gerenciamento de provisão e log de configurações

---
+++ examples/configuration_portal/main.cpp.ino
@@ -0,0 +1,82 @@
+#include <WiFi.h>
+#include "Core/Provisioning/ProvisioningManager.h"
+#include "Platform/Arduino/Provisioning/WebPortalProvisioningChannel.h"
+// #include "Platform/Arduino/Provisioning/BleProvisioningChannel.h"
+#include "Infra/Settings/ConfigManager.h"
+#include "Core/Providers/ServiceManager.h"
+#include "Platform/Arduino/Logging/ArduinoSerialLogger.h"
+#include "SmartSysApp.h"
+
+using namespace iotsmartsys::core::provisioning;
+using namespace iotsmartsys::core;
+
+ProvisioningManager provManager;
+WebPortalProvisioningChannel portalChannel;
+
+static iotsmartsys::SmartSysApp app;
+// BleProvisioningChannel bleChannel;
+
+static iotsmartsys::platform::arduino::ArduinoSerialLogger logger_(Serial);
+auto &sp_ = iotsmartsys::core::ServiceManager::init();
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(5000);
+    sp_.setLogLevel(iotsmartsys::core::LogLevel::Debug);
+    provManager.registerChannel(portalChannel);
+    // provManager.registerChannel(bleChannel);
+    provManager.onProvisioningCompleted([](const DeviceConfig &cfg)
+                                        {
+
+            logger_.info("Provisioning completed callback invoked.");
+            
+            auto &sm = sp_.settingsManager();
+
+                iotsmartsys::core::settings::Settings newSettings;
+                newSettings.wifi.ssid = cfg.wifi.ssid ? cfg.wifi.ssid : "";
+                newSettings.wifi.password = cfg.wifi.password ? cfg.wifi.password : "";
+                newSettings.api.key = cfg.deviceApiKey ? cfg.deviceApiKey : "";
+                newSettings.api.basic_auth = cfg.basicAuth ? cfg.basicAuth : "";
+                logger_.info("Saving new settings via SettingsManager.");
+                sm.save(newSettings); });
+
+    auto &settingsManager_ = sp_.settingsManager();
+    const auto cacheErr = settingsManager_.init();
+    logger_.info("SettingsManager init() completed with result %d", (int)cacheErr);
+
+    iotsmartsys::core::settings::Settings settings_;
+    if (cacheErr == iotsmartsys::core::common::StateResult::Ok)
+    {
+        if (settingsManager_.copyCurrent(settings_))
+        {
+            logger_.info("[SettingsManager] Loaded settings from NVS cache.");
+            logger_.warn("WiFi SSID='%s'", settings_.wifi.ssid.c_str());
+            logger_.warn("WiFi Password='%s'", settings_.wifi.password.c_str());
+            logger_.warn("API Key='%s'", settings_.api.key.c_str());
+            logger_.warn("API Basic Auth='%s'", settings_.api.basic_auth.c_str());
+            logger_.warn("MQtt Broker Host='%s'", settings_.mqtt.primary.host.c_str());
+            logger_.warn("MQtt Broker Port=%d", settings_.mqtt.primary.port);
+            logger_.warn("MQtt Broker User='%s'", settings_.mqtt.primary.user.c_str());
+            logger_.warn("MQtt Broker Password='%s'", settings_.mqtt.primary.password.c_str());
+            logger_.warn("MQtt Broker TTL=%d", settings_.mqtt.primary.ttl);
+            logger_.warn("NIVEL DE LOG ATUAL: %s", settings_.logLevelStr());
+        }
+        else
+        {
+            logger_.warn("[SettingsManager] Failed to copy current settings from NVS.");
+        }
+    }
+    else
+    {
+        logger_.warn("[SettingsManager] No cached settings found (or load failed). Using compile-time defaults until API refresh.");
+    }
+
+    provManager.begin();
+}
+
+void loop()
+{
+    provManager.loop();
+    // resto do app...
+}
