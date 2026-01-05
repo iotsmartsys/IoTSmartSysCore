0457b56ce3b2cbb8c19dfdc2718a5701172956cb
Marcelo Miranda <marceloc312@gmail.com>
2026-01-05 16:58:59 -0300

    feat: Recria o arquivo pins.h com definições de pinos para diferentes placas Espressif

---
+++ src/pins.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Platform/Espressif/Pinouts/ESP32_S3_Pinouts.h"
+#include "Platform/Espressif/Pinouts/ESP32_WROOM_32_Pinouts.h"
+#include "Platform/Espressif/Pinouts/ESP32_C6_Pinouts.h"
+#include "Platform/Espressif/Pinouts/ESP32_C3_Pinouts.h"
+#include "Platform/Espressif/Pinouts/ESP8266_Pinouts.h"
